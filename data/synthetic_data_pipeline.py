3a9dc643e8531b250f99fb58cb6fc0cc3c7b548e
hungphongtrn <hahuyhoanghhh41@gmail.com>
2024-07-11 08:59:16 +0000

    fix: cant save during process synthetic pipeline

--- data/synthetic_data_pipeline.py
+++ data/synthetic_data_pipeline.py
@@ -1,9 +1,11 @@
 import os
+import json
 import warnings
+import time
 from math import ceil
-from concurrent.futures import ProcessPoolExecutor, as_completed
 from multiprocessing import Queue, Process, Value
 from typing import List
+from queue import Empty
 
 import torch
 import torchaudio
@@ -22,60 +24,56 @@ logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
 
 @torch.no_grad()
 def process_text(
-    text: str,
-    index: int,
-    device: str = "cuda:0",
+    subset: Dataset,
+    device: str,
+    process_id: int,
+    queue: Queue,
     sample_rate: int = 24_000,
     max_retries: int = 3,
 ):
     """Convert text to audio and tokenize the audio.
-    
+
     Args:
-        text (str): The text to convert to audio.
-        index (int): The index of the example.
+        example (Dataset): The dataset containing the text to convert.
         device (str, optional): The device to use for processing. Defaults to "cuda:0".
         sample_rate (int, optional): The sample rate of the audio. Defaults to 24_000.
         max_retries (int, optional): The maximum number of retries. Defaults to 3.
-    
+
     Returns:
         dict: A dictionary containing the index, audio, and tokens or None if all attempts failed.
     """
-    logger.info(f"Processing index {index}")
+    logger.info(f"Process {process_id} will process {len(subset)} examples.")
+    batch_prompt = subset["prompt"]
+    batch_index = subset["index"]
     tts_processor = TTSProcessor(device=device)
     audio_tokenizer = AudioTokenizer(device=device)
-    for attempt in range(max_retries):
-        try:
-            audio = tts_processor.convert_text_to_audio(text).cpu()
-            audio_tokens = audio_tokenizer.process_single_audio((audio, sample_rate))
-            output = {"index": index, "audio": audio, "tokens": audio_tokens}
-        except Exception as e:
-            logger.warning(f"Attempt {attempt + 1} failed for index {index}: {str(e)}")
-            if attempt == max_retries - 1:
-                logger.error(f"All attempts failed for index {index}")
-                output = None
-    
-    del tts_processor
-    del audio_tokenizer
-    torch.cuda.empty_cache()
-    return output
+    for text, index in tqdm(
+        zip(batch_prompt, batch_index),
+        total=len(subset),
+        desc=f"Process {process_id}",
+        position=process_id,
+    ):
+        for attempt in range(max_retries):
+            try:
+                audio = tts_processor.convert_text_to_audio(text).cpu()
+                audio_tokens = audio_tokenizer.process_single_audio((audio, sample_rate))
+                queue.put({"index": index, "audio": audio, "tokens": audio_tokens})
+                break
+            except Exception as e:
+                logger.warning(f"Attempt {attempt + 1} failed for index {index}: {str(e)}")
+                if attempt == max_retries - 1:
+                    logger.error(f"All attempts failed for index {index}")
 
 
-def save_audio_tokens(
-    queue: Queue, audio_dir: str, token_dir: str, saved_count
-):
-    """Save audio and tokens to disk.
-    
+def save_batch(batch, audio_dir: str, token_dir: str, saved_count):
+    """Save a batch of audio and tokens to disk.
+
     Args:
-        queue (Queue): The queue containing the audio tokens.
+        batch (List[dict]): The batch containing the audio tokens.
         audio_dir (str): The directory to save the audio.
         token_dir (str): The directory to save the tokens.
-        total_examples (int): The total number of examples.
     """
-    while True:
-        audio_token = queue.get()
-        if audio_token is None:
-            break
-
+    for audio_token in batch:
         index = audio_token["index"]
         audio = audio_token["audio"]
         tokens = audio_token["tokens"]
@@ -86,67 +84,96 @@ def save_audio_tokens(
 
         torchaudio.save(audio_path, audio, 24_000)
         torch.save(tokens, token_path)
-        with saved_count.get_lock():
-            saved_count.value += 1
+    saved_count.value += len(batch)
+
+
+def save_audio_tokens(
+    queue: Queue,
+    audio_dir: str,
+    token_dir: str,
+    saved_count: Value,
+    queue_timeout: int = 5,
+    batch_size: int = 10,
+):
+    """Save audio and tokens to disk.
+
+    Args:
+        queue (Queue): The queue containing the audio tokens.
+        audio_dir (str): The directory to save the audio.
+        token_dir (str): The directory to save the tokens.
+        saved_count (Value): The shared value to keep track of the number of saved examples.
+    """
+    batch = []
+    while True:
+        logger.info(f"Saved {saved_count.value} examples.")
+        time.sleep(queue_timeout)
+        try:
+            audio_token = queue.get()
+            if audio_token is None:
+                break
+
+            batch.append(audio_token)
+            if len(batch) >= batch_size:
+                save_batch(batch, audio_dir, token_dir)
+                batch = []
+
+        except Empty:
+            if batch:
+                save_batch(batch, audio_dir, token_dir)
+                batch = []
 
 
 def run_pipeline(
     dataset: Dataset,
-    devices: List[str] = ["cuda:0"],
-    num_procs_per_device: int = 5,
-    audio_dir: str = "./audio",
-    token_dir: str = "./tokens",
+    devices: List[str] = ["cuda:0", "cuda:1", "cuda:2", "cuda:3"],
+    num_procs_per_device: int = 10,
+    audio_dir: str = "./new_audio",
+    token_dir: str = "./new_tokens",
 ):
     """Run the pipeline to convert text to audio and tokenize the audio.
-    
+
     Args:
         dataset (Dataset): The dataset to process.
         devices (List[str], optional): The devices to use for processing. Defaults to ["cuda:0"].
-        num_procs_per_device (int, optional): The number of processes per device. Defaults to 8.
-        audio_dir (str, optional): The directory to save the audio. Defaults to "./audio".
-        token_dir (str, optional): The directory to save the tokens. Defaults to "./tokens".
+        num_procs_per_device (int, optional): The number of processes per device. Defaults to 2.
+        audio_dir (str, optional): The directory to save the audio. Defaults to "./new_audio".
+        token_dir (str, optional): The directory to save the tokens. Defaults to "./new_tokens".
     """
     os.makedirs(audio_dir, exist_ok=True)
     os.makedirs(token_dir, exist_ok=True)
 
-    num_processes = len(devices) * num_procs_per_device
-    chunk_size = ceil(len(dataset) / num_processes)
-    chunks = []
-    for i in range(0, len(dataset), chunk_size):
-        if i + chunk_size <= len(dataset):
-            chunks.append(dataset.select(range(i, i + chunk_size)))
-        else:
-            chunks.append(dataset.select(range(i, len(dataset))))
+    num_workers = len(devices) * num_procs_per_device
+    logger.info(f"Using devices: {devices}")
+    logger.info(f"Running pipeline with {num_workers} workers.")
+    logger.info(f"New audio will be saved to {audio_dir}.")
+    logger.info(f"New tokens will be saved to {token_dir}.")
+
+    chunk_size = ceil(len(dataset) / num_workers)
+    chunks = [dataset.select(range(i, min(i + chunk_size, len(dataset)))) for i in range(0, len(dataset), chunk_size)]
 
     queue = Queue()
-    saved_count = Value('i', 0)
-    save_process = Process(
-        target=save_audio_tokens, args=(queue, audio_dir, token_dir, saved_count)
-    )
+    saved_count = Value("i", 0)
+    save_process = Process(target=save_audio_tokens, args=(queue, audio_dir, token_dir, saved_count))
     save_process.start()
 
-    with tqdm(total=len(dataset), position=0) as pbar:
-        with ProcessPoolExecutor(max_workers=num_processes) as executor:
-            futures = []    
-            for i, chunk in enumerate(chunks):
-                device = devices[i % len(devices)]
-                for example in chunk:
-                    text = example["prompt"]
-                    index = example["index"]
-                    future = executor.submit(process_text, text, index, device)
-                    futures.append(future)
+    worker_processes = []
+    for i, chunk in enumerate(chunks):
+        device = devices[i % len(devices)]
+        p = Process(target=process_text, args=(chunk, device, i, queue))
+        p.start()
+        worker_processes.append(p)
 
-            for future in as_completed(futures):
-                result = future.result()
-                pbar.update(1)
-                pbar.set_postfix({'Saved': saved_count.value}, refresh=True)
-                if result is not None:
-                    queue.put(result)
+    for p in worker_processes:
+        p.join()
 
     queue.put(None)
     save_process.join()
 
 
 if __name__ == "__main__":
+    with open("/home/root/Workspace/synthetic_data_generation/sound_instruct_llama3/data/remaining_indices.json") as f:
+        remaining_indices = json.load(f)
+
+    logger.info(f"Continue process for {len(remaining_indices)} examples")
     dataset = load_dataset("jan-hq/instruction-speech-v1.5", split="train")
-    run_pipeline(dataset.select(range(100)))
+    run_pipeline(dataset.select(remaining_indices))
