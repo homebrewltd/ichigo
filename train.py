24ed084eadfc4927b8aae9bfc8ffba8bfedbf9c8
Hoang Ha <64120343+hahuyhoang411@users.noreply.github.com>
2024-07-18 15:01:01 +0700

    add warm up

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ print_once("___________________________________")
 
 # Model loading
 print_once("--- Load Model ---")
-model_path = "jan-hq/llama-3-sound-init"
+model_path = "jan-hq/Jan-Llama3-0708"
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
     torch_dtype=torch.bfloat16,
@@ -53,38 +53,12 @@ print_once("--- Initialization complete ---")
 
 # Setting up data train
 dataset_train = load_dataset(
-    "jan-hq/instruction-speech-conversation-interleaved",
+    "jan-hq/instruction-speech-conversation-v1.5-phase-2-sound-convo",
     num_proc=num_cores,
     split="train",
 )
-# def formatting_prompts_func(examples, column_name):
-#     convos = examples[column_name]
-#     texts = [tokenizer.apply_chat_template(convo, tokenize=False, add_generation_prompt=False) for convo in convos]
-#     return {"text": texts}
 
-# dataset = load_dataset("jan-hq/instruction-speech-conversation", num_proc=num_cores, split="train")
-# columns_to_remove = [col for col in dataset.column_names if col not in ["sound_convo", "text_convo"]]
-# dataset = dataset.remove_columns(columns_to_remove)
-# print_once(dataset)
-
-# dataset_sound = dataset.map(
-#     lambda x: formatting_prompts_func(x, "sound_convo"),
-#     batched=True,
-#     num_proc=num_cores,
-#     batch_size=10000
-# ).remove_columns(["sound_convo","text_convo"])
-# print_once(dataset_sound)
-
-# dataset_text = dataset.map(
-#     lambda x: formatting_prompts_func(x, "text_convo"),
-#     batched=True,
-#     num_proc=num_cores,
-#     batch_size=10000
-# ).remove_columns(["sound_convo","text_convo"])
-# print_once(dataset_text)
-
-# dataset_train = interleave_datasets([dataset_sound, dataset_text], probabilities=[0.8, 0.2], seed=42, stopping_strategy="first_exhausted")
-
+print_once("--- Dataset loading ---")
 print_once("___________________________________")
 print_once(dataset_train)
 print_once("-----------------------------------")
@@ -93,33 +67,8 @@ print_once("-----------------------------------")
 print_once(dataset_train[200]["text"][:100])
 print_once("___________________________________")
 
-
-# class CustomSFTTrainer(SFTTrainer):
-#     def create_optimizer_and_scheduler(self, num_training_steps: int):
-#         config = AutoConfig.from_pretrained(self.model.config.name_or_path)
-
-#         self.optimizer = Adam_mini(
-#             model=self.model,
-#             lr=self.args.learning_rate,
-#             weight_decay=self.args.weight_decay,
-#             beta1=0.9,
-#             beta2=0.98,
-#             epsilon=1e-6,
-#             zero_3=True,
-#             n_embd=config.hidden_size,
-#             n_head=config.num_attention_heads,
-#             n_query_groups=config.num_key_value_heads,  # GQA
-#         )
-
-#         self.lr_scheduler = get_cosine_schedule_with_warmup(
-#             self.optimizer,
-#             num_warmup_steps=int(num_training_steps * 0.1),  # 10% warmup
-#             num_training_steps=num_training_steps,
-#         )
-
-
 # Training args
-per_device_train_batch_size = 3
+per_device_train_batch_size = 4
 num_train_epochs = 1
 gradient_accumulation_steps = 4
 
@@ -130,7 +79,6 @@ print_once(f"{'Gradient Accumulation Steps:':30} {gradient_accumulation_steps}")
 
 config = AutoConfig.from_pretrained(model_path)
 gpu_count = torch.cuda.device_count()
-
 
 def training_step_calc(
     dataset, batch_size, num_gpus, num_epochs, gradient_accumulation_steps
@@ -151,8 +99,11 @@ training_steps = training_step_calc(
 )
 
 save_steps = int(training_steps // 80)
+warmup_steps = int(training_steps*0.05)
+
 print_once(f"{'Training steps':30} {training_steps}")
 print_once(f"{'Saving steps':30} {save_steps}")
+print_once(f"{'Warming steps':30} {warmup_steps}")
 print_once("___________________________________")
 
 # Create the custom trainer
@@ -173,6 +124,7 @@ trainer = SFTTrainer(
         save_strategy="steps",
         save_steps=save_steps,
         save_total_limit=5,
+        warmup_steps=warmup_steps,
         learning_rate=5e-5,
         weight_decay=0.01,
         seed=3407,
