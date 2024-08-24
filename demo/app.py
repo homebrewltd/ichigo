97994ed7b8d131a4911d88ba8c461405475c6e71
bachvudinh <bachvudinh02@gmail.com>
2024-08-24 08:29:40 +0000

    refactor gradio demo code and add instruction for user to host demo locally

--- demo/app.py
+++ demo/app.py
@@ -9,12 +9,30 @@ from transformers import StoppingCriteria, StoppingCriteriaList, TextIteratorStr
 from threading import Thread
 import logging
 import os
-from generate_audio import (
+from .generate_audio import (
     TTSProcessor,
 )  
 import uuid
+import argparse
+from huggingface_hub import hf_hub_download
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Host the Gradio WebUI with custom settings")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host address to run the server on")
+    parser.add_argument("--port", type=int, default=7860, help="Port to run the server on")
+    parser.add_argument("--max-seq-len", type=int, default=1024, help="Maximum sequence length for generation")
+    parser.add_argument("--use-8bit", action="store_true", help="Use 8-bit quantization")
+    parser.add_argument("--use-4bit", action="store_true", help="Use 4-bit quantization")
+    return parser.parse_args()
+args = parse_arguments()
+
 
 device = "cuda"
+if not os.path.exists("whisper-vq-stoks-medium-en+pl-fixed.model"):
+    hf_hub_download(
+        repo_id="jan-hq/WhisperVQ",
+        filename="whisper-vq-stoks-medium-en+pl-fixed.model",
+        local_dir=".",
+    )
 vq_model = RQBottleneckTransformer.load_model(
         "whisper-vq-stoks-medium-en+pl-fixed.model"
     ).to(device)
@@ -24,14 +42,22 @@ use_8bit = False
 llm_path = "homebrewltd/llama3.1-s-instruct-v0.2"
 tokenizer = AutoTokenizer.from_pretrained(llm_path)
 model_kwargs = {}
-if use_8bit:
+if args.use_8bit:
     model_kwargs["quantization_config"] = BitsAndBytesConfig(
         load_in_8bit=True,
         llm_int8_enable_fp32_cpu_offload=False,
         llm_int8_has_fp16_weight=False,
     )
+elif args.use_4bit:
+        model_kwargs["quantization_config"] = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+        )
 else:
     model_kwargs["torch_dtype"] = torch.bfloat16
+
 model = AutoModelForCausalLM.from_pretrained(llm_path, **model_kwargs).to(device)
 
 def audio_to_sound_tokens_whisperspeech(audio_path):
@@ -40,7 +66,7 @@ def audio_to_sound_tokens_whisperspeech(audio_path):
     if sr != 16000:
         wav = torchaudio.functional.resample(wav, sr, 16000)
     with torch.no_grad():
-        codes = vq_model.encode_audio(wav.to(device))
+        codes = vq_model.encode_audio(wav.to('cuda'))
         codes = codes[0].cpu().tolist()
     
     result = ''.join(f'<|sound_{num:04d}|>' for num in codes)
@@ -51,7 +77,7 @@ def audio_to_sound_tokens_whisperspeech_transcribe(audio_path):
     if sr != 16000:
         wav = torchaudio.functional.resample(wav, sr, 16000)
     with torch.no_grad():
-        codes = vq_model.encode_audio(wav.to(device))
+        codes = vq_model.encode_audio(wav.to('cuda'))
         codes = codes[0].cpu().tolist()
     
     result = ''.join(f'<|sound_{num:04d}|>' for num in codes)
@@ -74,23 +100,12 @@ def audio_to_sound_tokens(audio_path, target_bandwidth=1.5, device="cuda"):
     result = ''.join(f'<|sound_{num:04d}|>' for num in flatten_tokens)
     return f'<|sound_start|>{result}<|sound_end|>'
 
-def setup_pipeline(model_path, use_4bit=False, use_8bit=False):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model_kwargs = {"device_map": "auto"}
-    if use_8bit:
-        model_kwargs["quantization_config"] = BitsAndBytesConfig(
-            load_in_8bit=True,
-            llm_int8_enable_fp32_cpu_offload=False,
-            llm_int8_has_fp16_weight=False,
-        )
-    else:
-        model_kwargs["torch_dtype"] = torch.bfloat16
-    model = AutoModelForCausalLM.from_pretrained(model_path, **model_kwargs)
-    return pipeline("text-generation", model=model, tokenizer=tokenizer)
-
 def text_to_audio_file(text):
     # gen a random id for the audio file
     id = str(uuid.uuid4())
+    # create a user audio folder to store the audio files if it does not exist
+    if not os.path.exists("./user_audio"):
+        os.makedirs("./user_audio")
     temp_file = f"./user_audio/{id}_temp_audio.wav"
     text = text
     text_split = "_".join(text.lower().split(" "))  
@@ -112,8 +127,8 @@ def process_transcribe_input(audio_file=None):
     
 class StopOnTokens(StoppingCriteria):
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-        # encode </s> token
-        stop_ids = [tokenizer.eos_token_id, 128009]  # Adjust this based on your model's tokenizer
+
+        stop_ids = [tokenizer.eos_token_id, 128009]  # 128009 is the end of turn token
         for stop_id in stop_ids:
             if input_ids[0][-1] == stop_id:
                 return True
@@ -140,7 +155,7 @@ def process_audio(audio_file, transcript=False):
     generation_kwargs = dict(
         input_ids=input_ids,
         streamer=streamer,
-        max_new_tokens=1024,
+        max_new_tokens=args.max_seq_len,
         do_sample=False,
         stopping_criteria=StoppingCriteriaList([stop])
     )
@@ -156,22 +171,11 @@ def process_audio(audio_file, transcript=False):
         partial_message = partial_message.replace("assistant\n\n", "")
         yield partial_message
 
-good_examples = []
-for file in os.listdir("./examples"):
-    if file.endswith(".wav"):
-        good_examples.append([f"./examples/{file}"])
-bad_examples = []
-for file in os.listdir("./bad_examples"):
-    if file.endswith(".wav"):
-        bad_examples.append([f"./bad_examples/{file}"])
-examples = []
-examples.extend(good_examples)
-examples.extend(bad_examples)
-
 with gr.Blocks() as iface:
     gr.Markdown("# Llama3-1-S: checkpoint Aug 20, 2024")
     gr.Markdown("Enter text to convert to audio, then submit the audio to generate text or Upload Audio")
-    
+    gr.Markdown("Powered by [Homebrew Ltd](https://homebrew.ltd/) | [Read our blog post](https://homebrew.ltd/blog/llama3-just-got-ears)")
+
     with gr.Row():
         input_type = gr.Radio(["text", "audio"], label="Input Type", value="audio")
         text_input = gr.Textbox(label="Text Input", visible=False)
@@ -214,8 +218,6 @@ with gr.Blocks() as iface:
         outputs=[text_output]
     )
     
-    gr.Examples(examples, inputs=[audio_input])
 iface.queue(max_size=10)
-# iface.launch(server_name="127.0.0.1", server_port=8080)
 # launch locally
-iface.launch(server_name="0.0.0.0")
+iface.launch(server_name=args.host, server_port=args.port)
